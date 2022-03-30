786fc95d7ff82494753ab7e9a3031589f96558ed
Rebecca <rebfje-1@student.ltu.se>
2022-03-30 14:30:34 +0200

    ändring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 2;
 
     System.out.println("hello world");
+    System.out.println(number1+number2);
 
     }
 }
